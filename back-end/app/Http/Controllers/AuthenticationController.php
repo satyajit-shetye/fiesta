cebc5879733df24c6d95bcdd2dbc9ba3e06524d6
sshetye08 <sshetye08@gmail.com>
2020-12-26 15:49:11 +0530

    Signup API

--- back-end/app/Http/Controllers/AuthenticationController.php
+++ back-end/app/Http/Controllers/AuthenticationController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
-    function signup (){
-        return 'SignUp';
+    function signup (Request $request){
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string'],
+            'birthdate' => ['required', 'date'],
+            'gender' => ['required', 'boolean'],
+        ],$messages = [
+            'boolean' => 'The :attribute field is invalid.',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $user = User::create([
+            'email' => $input['email'],
+            'birth_date' => $input['birth_date'],
+            'gender' => $input['gender'],
+            'password' => Hash::make($input['password'])
+        ]);
+
+        return response()->json([
+            'response' => $user
+        ]);
     }
 
     function signin (){
--- back-end/app/Http/Kernel.php
+++ back-end/app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
@@ -38,9 +39,9 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
         'api' => [
-            'throttle:api',
+            EnsureFrontendRequestsAreStateful::class,
+            'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
